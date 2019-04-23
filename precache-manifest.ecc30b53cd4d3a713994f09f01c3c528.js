17eac2a3a8a9b846eba67ec92cd8523ff5d48c08
Britz Wang <britz.wang@kooppi.com>
2019-04-23 21:47:15 +0800

    20190422

---
+++ precache-manifest.ecc30b53cd4d3a713994f09f01c3c528.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e07a6903f80681569d2c99fddd47633",
+    "url": "/static/media/skill-pic.8e07a690.jpg"
+  },
+  {
+    "revision": "a4aec994dd09476d01c198edcf3e570d",
+    "url": "/static/media/me2.a4aec994.jpg"
+  },
+  {
+    "revision": "d117ba61afbe9fab1786a171897cf8f8",
+    "url": "/static/media/me.d117ba61.jpg"
+  },
+  {
+    "revision": "7b26e6fd9d9f784d722aa2d9c99ce520",
+    "url": "/static/media/experience-pic.7b26e6fd.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d05b487d8e4d4cbc1ad6",
+    "url": "/static/js/main.1b21d97b.chunk.js"
+  },
+  {
+    "revision": "317e775898966d0e3a79",
+    "url": "/static/js/2.cec6252d.chunk.js"
+  },
+  {
+    "revision": "d05b487d8e4d4cbc1ad6",
+    "url": "/static/css/main.01792d41.chunk.css"
+  },
+  {
+    "revision": "835dc1691d9aadb0eb628c570e4b8acf",
+    "url": "/index.html"
+  }
+];
